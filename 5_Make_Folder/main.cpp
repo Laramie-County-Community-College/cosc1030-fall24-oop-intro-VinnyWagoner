1f07928132a92e75a3517dab643abed0ea2c1f90
VinnyWagoner <vincentwagoner@student.lccc.wy.edu>
2025-11-14 03:46:52 +0000

    I think it works

--- 5_Make_Folder/main.cpp
+++ 5_Make_Folder/main.cpp
@@ -21,14 +21,29 @@ int main() {
    Triangle triangle1;
    Triangle triangle2;
 
-   // TODO: Read and set base and height for triangle1 (use SetBase() and SetHeight())
-      
-   // TODO: Read and set base and height for triangle2 (use SetBase() and SetHeight())
+   double base;
+   double height;
+
+  // Read and set base and height for triangle1
+   cin >> base >> height;
+   triangle1.SetBase(base);
+   triangle1.SetHeight(height);
+
+   // Read and set base and height for triangle2
+   cin >> base >> height;
+   triangle2.SetBase(base);
+   triangle2.SetHeight(height);
        
    cout << "Triangle with smaller area:" << endl;
    
-   // TODO: Determine smaller triangle (use GetArea())  
-   //       and output smaller triangle's info (use PrintInfo())
+   // Determine smaller triangle and output its info
+   if (triangle1.GetArea() < triangle2.GetArea()) {
+      triangle1.PrintInfo();
+   }
+   else {
+      triangle2.PrintInfo();
+   }
+
    
    return 0;
 }
